982615f444c58e8ab801fecdb5800f6ac9d1419d
BryanJChuquimarca <bryanchuquimarcacastillo@gmail.com>
2026-02-04 21:26:35 +0100

    alert basico

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,7 +3,13 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ToastController } from '@ionic/angular';
-import { IonContent, IonButton, IonCardContent, IonCard, IonIcon } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonButton,
+  IonCardContent,
+  IonCard,
+  IonIcon,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
@@ -11,24 +17,23 @@ import { IonContent, IonButton, IonCardContent, IonCard, IonIcon } from '@ionic/
   styleUrls: ['home.page.scss'],
   imports: [IonContent, IonButton, IonCardContent, IonCard, IonIcon],
 })
-
 export class HomePage {
-
   deckId: string = '';
   playerCards: any[] = [];
   crupierCards: any[] = [];
   crupierOcultar: boolean = false;
 
-
-
   balance: number = 1000;
-  apuesta_actual: number = 0
+  apuesta_actual: number = 0;
   wins: number = 0;
-  losses: number = 0
+  losses: number = 0;
   partidas_jugadas: number = 0;
   partida_en_curso: boolean = false;
 
-  constructor(private http: HttpClient, private toastController: ToastController) { }
+  constructor(
+    private http: HttpClient,
+    private toastController: ToastController,
+  ) {}
 
   async ngOnInit() {
     this.loadState();
@@ -36,15 +41,19 @@ export class HomePage {
   }
 
   async crear_mazo() {
-
-    const response = await this.http.get<any>('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').toPromise();
+    const response = await this.http
+      .get<any>('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+      .toPromise();
     this.deckId = response.deck_id;
   }
 
   async drawCards(count: number) {
-    const response = await this.http.get<any>(`https://www.deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=${count}`).toPromise();
+    const response = await this.http
+      .get<any>(
+        `https://www.deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=${count}`,
+      )
+      .toPromise();
     return response.cards;
-
   }
 
   async ponerApuesta(cantidad: number) {
@@ -65,60 +74,54 @@ export class HomePage {
 
   async repartir() {
     if (this.apuesta_actual === 0) {
-      const toast = await this.toastController.create({
-        message: 'No puedes jugar sin apostar',
-        duration: 1500,
-        position: 'top',
-      });
-
-      await toast.present();
+      alert(
+        '⚠️ Debes hacer una apuesta antes de jugar.\n\nSelecciona una ficha para apostar.',
+      );
       return;
-    };
+    }
     await this.crear_mazo();
-    
+
     this.playerCards = await this.drawCards(2);
     this.crupierCards = await this.drawCards(2);
     this.crupierOcultar = true;
     this.partida_en_curso = true;
-    console.log(this.playerCards, this.crupierCards)
+    console.log(this.playerCards, this.crupierCards);
 
-    //com0probar si hay blackjack natural, 
+    //com0probar si hay blackjack natural,
     let playerTotal = this.calculateTotal(this.playerCards);
     let crupierTotal = this.calculateTotal(this.crupierCards);
     let playerNatural = playerTotal == 21 && this.playerCards.length == 2;
     let crupierNatural = crupierTotal == 21 && this.crupierCards.length == 2;
-    console.log(playerNatural)
+    console.log(playerNatural);
     if (playerNatural || crupierNatural) {
       this.crupierOcultar = false;
       if (playerNatural && !crupierNatural) {
-        await this.hasganado(true, 'top')
+        await this.hasganado(true, 'top');
         return;
       } else if (!playerNatural && crupierNatural) {
-        await this.hasperdido(false, 'top')
+        await this.hasperdido(false, 'top');
         return;
       } else if (playerNatural && crupierNatural) {
-        await this.empate(null, 'top')
+        await this.empate(null, 'top');
         return;
       }
     }
   }
 
   async hit() {
-    this.playerCards.push(...await this.drawCards(1));
+    this.playerCards.push(...(await this.drawCards(1)));
     if (this.calculateTotal(this.playerCards) > 21) {
-      this.hasperdido(false, 'top')
+      this.hasperdido(false, 'top');
     }
   }
 
-
   async doubleDown() {
-
     if (this.playerCards.length === 2 && this.apuesta_actual <= this.balance) {
       this.balance -= this.apuesta_actual;
       this.apuesta_actual *= 2;
-      this.playerCards.push(...await this.drawCards(1));
+      this.playerCards.push(...(await this.drawCards(1)));
       if (this.calculateTotal(this.playerCards) > 21) {
-        this.hasperdido(false, 'top')
+        this.hasperdido(false, 'top');
       } else {
         await this.stand();
       }
@@ -132,31 +135,36 @@ export class HomePage {
     }
   }
 
-
   async stand() {
     this.crupierOcultar = false;
 
-    while (this.calculateTotal(this.crupierCards) < this.calculateTotal(this.playerCards) && this.calculateTotal(this.crupierCards) < 21) { //mirar si hay empates
-      this.crupierCards.push(...await this.drawCards(1));
+    while (
+      this.calculateTotal(this.crupierCards) <
+        this.calculateTotal(this.playerCards) &&
+      this.calculateTotal(this.crupierCards) < 21
+    ) {
+      //mirar si hay empates
+      this.crupierCards.push(...(await this.drawCards(1)));
     }
 
     let playerTotal = this.calculateTotal(this.playerCards);
     let crupierTotal = this.calculateTotal(this.crupierCards);
 
     if (crupierTotal > 21 || playerTotal > crupierTotal) {
-      this.hasganado(true, 'top')
+      this.hasganado(true, 'top');
     } else if (playerTotal < crupierTotal) {
-      this.hasperdido(false, 'top')
+      this.hasperdido(false, 'top');
     } else {
-      this.empate(null, 'top')
+      this.empate(null, 'top');
     }
   }
 
   calculateVisibleTotal(): number {
     if (!this.crupierCards || this.crupierCards.length === 0) return 0;
 
-
-    let visibleCards = this.crupierOcultar ? this.crupierCards.slice(1) : this.crupierCards;
+    let visibleCards = this.crupierOcultar
+      ? this.crupierCards.slice(1)
+      : this.crupierCards;
     return this.calculateTotal(visibleCards);
   }
 
@@ -189,7 +197,6 @@ export class HomePage {
     if (playerWon === true) {
       this.balance += this.apuesta_actual * 2;
       this.wins++;
-
     } else if (playerWon === false) {
       this.balance -= this.apuesta_actual;
     }
@@ -213,7 +220,6 @@ export class HomePage {
     if (playerWon === true) {
       this.balance += this.apuesta_actual;
       this.wins++;
-
     }
 
     this.apuesta_actual = 0;
@@ -234,7 +240,6 @@ export class HomePage {
     if (playerWon === true) {
       this.balance += this.apuesta_actual;
       this.wins++;
-
     } else if (playerWon === false) {
       this.balance -= this.apuesta_actual;
     }
@@ -255,7 +260,7 @@ export class HomePage {
     const state = {
       balance: this.balance,
       wins: this.wins,
-      partidas_jugadas: this.partidas_jugadas
+      partidas_jugadas: this.partidas_jugadas,
     };
     localStorage.setItem('blackjack_state', JSON.stringify(state));
   }
@@ -279,8 +284,5 @@ export class HomePage {
     if (this.balance <= 9) {
       this.balance += 10;
     }
-
   }
-
 }
-
